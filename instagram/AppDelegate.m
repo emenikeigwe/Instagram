05af786d9b3f53cdca343daa286b55d2d37419de
Ezi Godfreey-Igwe <arleneigwe@fb.com>
2019-07-08 04:42:12 -0700

    Changes made to pod file and appDelegate.m

--- instagram/AppDelegate.m
+++ instagram/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,13 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration   configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+        
+        configuration.applicationId = @"APP_ID";
+        configuration.server = @"http://finstainsta.herokuapp.com/parse";
+    }];
+    
+    [Parse initializeWithConfiguration:config];
     return YES;
 }
 
