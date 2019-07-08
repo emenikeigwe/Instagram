a17adf549628cc4f68e2ccfd4adfccc1ccc0318d
Ezi Godfreey-Igwe <arleneigwe@fb.com>
2019-07-08 04:15:27 -0700

    Initial Commit

---
+++ instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  instagram
+//
+//  Created by arleneigwe on 7/8/19.
+//  Copyright © 2019 arleneigwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
